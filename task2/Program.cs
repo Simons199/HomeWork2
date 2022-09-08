c7932ce9c4b0ac32bbeab881b4a8ae4b70f29b2c
semion <ifreeman2254@gmail.com>
2022-09-08 18:34:06 +0300

    task2

---
+++ task2/Program.cs
@@ -0,0 +1,29 @@
+﻿int num = ReadInt("Введите число: ");
+int count = num.ToString().Length;
+Console.Write(MakeArray(num, count));
+
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int MakeArray(int x, int y)
+{
+int result = 0;
+    if (y < 3)
+    {
+        Console.Write("Третьей цифры нет:");
+    }
+    else
+    {
+        int z = 1;
+        for (int i = y; i > 3; i--)
+        {
+            z = z * 10;
+        }
+
+        result = (x / z) % 10;
+    }
+return result;
+}
