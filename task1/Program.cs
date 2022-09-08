eaeb8d903c48316d795d9ddae1f303c68f43adc5
semion <ifreeman2254@gmail.com>
2022-09-08 17:14:02 +0300

    tsk1

---
+++ task1/Program.cs
@@ -0,0 +1,8 @@
+﻿//Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+Console.WriteLine("Введите трехзначное число");
+int num = Convert.ToInt32(Console.ReadLine());
+{
+int result = (num / 10) % 10;
+System.Console.WriteLine($"Вторая цифра числа: {Math.Abs(result)}");
+}
